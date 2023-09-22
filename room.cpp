87ecd39562ce63a5c99201a322e7c1248457b1e2
曲奇Onfree <2098332747@qq.com>
2023-09-22 21:16:47 +0800

    完善群聊class BaseGroupRoom，加入提示和单人发送功能

--- room.cpp
+++ room.cpp
@@ -19,18 +19,9 @@ namespace room
 
         std::lock_guard<std::shared_mutex> lock(m_userMap_mutex);
         if (m_userMap.find(socket_ptr) == m_userMap.end()) return false;
-        m_userMap.erase(m_userMap.find(socket_ptr));
+            m_userMap.erase(m_userMap.find(socket_ptr));
+
         return true;
-    }
-
-    bool BaseRoom::baseMuteMember(long long user_id, const std::chrono::minutes& minutes)
-    {
-        return false;
-    }
-
-    bool BaseRoom::baseUnmuteMember(long long user_id)
-    {
-        return false;
     }
 
     asio::awaitable<bool> BaseRoom::baseSendData(const std::string& data)
@@ -38,12 +29,13 @@ namespace room
         bool queueEmpty = true;
         // 广播数据
         {
+            qcrypto::AES<qcrypto::AESMode::CBC_256> aes;
             std::shared_lock<std::shared_mutex> sharedLock(m_userMap_mutex);
+
             for (auto i = m_userMap.begin(); i != m_userMap.end(); i++)
             {
                 try
                 {
-                    qcrypto::AES<qcrypto::AESMode::CBC_256> aes;
                     std::string out;
                     if (!aes.encrypt(data, out, { i->second.key, 32 }, { i->second.iv, 16 }, true))
                         throw std::logic_error("Key and ivec of AES are invalid");
@@ -52,6 +44,55 @@ namespace room
                 catch (...)
                 {
                     m_userDeleteQueue.push(i->first);
+                }
+            }
+            queueEmpty = m_userDeleteQueue.empty();
+        }
+
+        // 去除已经关闭的连接
+        {
+            if (queueEmpty)
+            {
+                std::lock_guard<std::shared_mutex> lock(m_userMap_mutex);
+                while (!m_userDeleteQueue.empty())
+                {
+                    std::shared_ptr<asio::ip::tcp::socket> localSocket_ptr = std::move(m_userDeleteQueue.front());
+                    if (m_userMap.find(localSocket_ptr) != m_userMap.end())
+                    {
+                        m_userMap.erase(m_userMap.find(localSocket_ptr));
+                    }
+
+                    m_userDeleteQueue.pop();
+                }
+            }
+        }
+
+        co_return true;
+    }
+
+    asio::awaitable<bool> BaseRoom::baseSendData(const std::string& data, long long user_id)
+    {
+        bool queueEmpty = true;
+        // 广播数据
+        {
+            qcrypto::AES<qcrypto::AESMode::CBC_256> aes;
+            std::shared_lock<std::shared_mutex> sharedLock(m_userMap_mutex);
+
+            for (auto i = m_userMap.begin(); i != m_userMap.end(); i++)
+            {
+                if (i->second.user_id == user_id)
+                {
+                    try
+                    {
+                        std::string out;
+                        if (!aes.encrypt(data, out, { i->second.key, 32 }, { i->second.iv, 16 }, true))
+                            throw std::logic_error("Key and ivec of AES are invalid");
+                        co_await i->first->async_send(asio::buffer(out), asio::use_awaitable);
+                    }
+                    catch (...)
+                    {
+                        m_userDeleteQueue.push(i->first);
+                    }
                 }
             }
             queueEmpty = m_userDeleteQueue.empty();
@@ -86,12 +127,11 @@ namespace qls
     BasePrivateRoom::BasePrivateRoom(long long user_id_1, long long user_id_2) :
         m_user_id_1(user_id_1),
         m_user_id_2(user_id_2)
-    {
-    }
+    {}
 
     bool BasePrivateRoom::joinRoom(const std::shared_ptr<asio::ip::tcp::socket>& socket_ptr, const User& user)
     {
-        if (user.user_id != m_user_id_1 || user.user_id != m_user_id_2)
+        if (user.user_id != m_user_id_1 && user.user_id != m_user_id_2)
             return false;
         return joinBaseRoom(socket_ptr, user);
     }
@@ -101,39 +141,100 @@ namespace qls
         return leaveBaseRoom(socket_ptr);
     }
 
-    asio::awaitable<bool> BasePrivateRoom::sendData(const std::string& message, long long user_id)
+    asio::awaitable<bool> BasePrivateRoom::sendMessage(const std::string& message, long long sender_user_id)
     {
+        if (sender_user_id != m_user_id_1 && sender_user_id != m_user_id_2)
+            co_return false;
+
         qjson::JObject json;
         json["type"] = "private_message";
-        json["data"]["user_id"] = user_id;
+        json["data"]["user_id"] = sender_user_id;
         json["data"]["message"] = message;
-        std::string out;
 
         co_return co_await baseSendData(qjson::JWriter::fastWrite(json));
     }
 
+    asio::awaitable<bool> BasePrivateRoom::sendTipMessage(const std::string& message)
+    {
+        qjson::JObject json;
+        json["type"] = "private_tip_message";
+        json["data"]["message"] = message;
+
+        co_return co_await baseSendData(qjson::JWriter::fastWrite(json));
+    }
+
+    BaseGroupRoom::BaseGroupRoom(long long group_id) :
+        m_group_id(group_id) {}
+
     bool BaseGroupRoom::baseAddMember(long long user_id)
     {
-        return false;
+        std::lock_guard<std::shared_mutex> lg(m_user_id_map_mutex);
+        if (m_user_id_map.find(user_id) == m_user_id_map.end())
+            m_user_id_map.insert(user_id);
+
+        return true;
     }
 
     bool BaseGroupRoom::baseRemoveMember(long long user_id)
     {
-        return false;
+        std::lock_guard<std::shared_mutex> lg(m_user_id_map_mutex);
+        if (m_user_id_map.find(user_id) != m_user_id_map.end())
+            m_user_id_map.erase(user_id);
+
+        return true;
     }
 
     bool BaseGroupRoom::joinRoom(const std::shared_ptr<asio::ip::tcp::socket>& socket_ptr, const User& user)
     {
-        return false;
+        return joinBaseRoom(socket_ptr, user);
     }
 
     bool BaseGroupRoom::leaveRoom(const std::shared_ptr<asio::ip::tcp::socket>& socket_ptr)
     {
-        return false;
+        return leaveBaseRoom(socket_ptr);
     }
 
-    asio::awaitable<bool> BaseGroupRoom::sendData(const std::string& message, long long user_id)
+    asio::awaitable<bool> BaseGroupRoom::sendMessage(const std::string& message, long long sender_user_id)
     {
-        co_return false;
+        // 是否有此user_id
+        {
+            std::shared_lock<std::shared_mutex> sl(m_user_id_map_mutex);
+            if (m_user_id_map.find(sender_user_id) == m_user_id_map.end())
+                co_return false;
+        }
+
+        qjson::JObject json;
+        json["type"] = "group_message";
+        json["data"]["user_id"] = sender_user_id;
+        json["data"]["group_id"] = m_group_id;
+        json["data"]["message"] = message;
+
+        co_return co_await baseSendData(qjson::JWriter::fastWrite(json));
+    }
+
+    asio::awaitable<bool> BaseGroupRoom::sendTipMessage(const std::string& message)
+    {
+        qjson::JObject json;
+        json["type"] = "group_tip_message";
+        json["data"]["group_id"] = m_group_id;
+        json["data"]["message"] = message;
+
+        co_return co_await baseSendData(qjson::JWriter::fastWrite(json));
+    }
+
+    asio::awaitable<bool> BaseGroupRoom::sendUserTipMessage(const std::string& message, long long receiver_user_id)
+    {
+        {
+            std::shared_lock<std::shared_mutex> sl(m_user_id_map_mutex);
+            if (m_user_id_map.find(receiver_user_id) == m_user_id_map.end())
+                co_return false;
+        }
+
+        qjson::JObject json;
+        json["type"] = "group_tip_message";
+        json["data"]["group_id"] = m_group_id;
+        json["data"]["message"] = message;
+
+        co_return co_await baseSendData(qjson::JWriter::fastWrite(json), receiver_user_id);
     }
 }
--- room.h
+++ room.h
@@ -8,9 +8,9 @@
 #include <memory>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <queue>
 #include <chrono>
-#include "network.h"
 
 namespace room
 {
@@ -29,12 +29,6 @@ namespace room
             Ios
         };
 
-        enum class UserType
-        {
-            Default,
-            Operator
-        };
-
         struct BaseUserSetting
         {
             long long user_id;
@@ -43,17 +37,11 @@ namespace room
             char iv[16 + 1]{ 0 };
         };
 
-        class BaseUserInfo
-        {
-            long long user_id;
-            UserType  user_type = UserType::Default;
-        };
-
         BaseRoom() = default;
         ~BaseRoom() = default;
 
         /*
-        * @brief 加入房间
+        * @brief 用户连接加入房间
         * @param socket_ptr socket指针
         * @param user 用户数据
         * @return true成功 | false失败
@@ -61,19 +49,11 @@ namespace room
         bool joinBaseRoom(const std::shared_ptr<asio::ip::tcp::socket>& socket_ptr, const BaseUserSetting& user);
         
         /*
-        * @brief 离开房间
+        * @brief 用户连接离开房间
         * @param socket_ptr socket指针
         * @return true成功 | false失败
         */
         bool leaveBaseRoom(const std::shared_ptr<asio::ip::tcp::socket>& socket_ptr);
-        
-        /*
-        * @brief 禁言用户
-        * @brief 
-        */
-        bool baseMuteMember(long long user_id, const std::chrono::minutes& minutes);
-
-        bool baseUnmuteMember(long long user_id);
 
         /*
         * @brief 发送消息给类中所有用户（广播）
@@ -81,6 +61,14 @@ namespace room
         * @return 协程 true成功 | false失败
         */
         asio::awaitable<bool> baseSendData(const std::string& data);
+
+        /*
+        * @brief 发送消息给某用户
+        * @param data 二进制数据
+        * @param user_id 用户id
+        * @return 协程 true成功 | false失败
+        */
+        asio::awaitable<bool> baseSendData(const std::string& data, long long user_id);
 
     private:
         std::unordered_map<std::shared_ptr<asio::ip::tcp::socket>, BaseUserSetting>    m_userMap;
@@ -103,7 +91,7 @@ namespace qls
         ~BasePrivateRoom() = default;
 
         /*
-        * @brief 加入私聊房间
+        * @brief 用户连接加入私聊房间
         * @param socket_ptr socket指针
         * @param user 用户数据
         * @return true 加入成功 | false 加入失败
@@ -111,7 +99,7 @@ namespace qls
         bool joinRoom(const std::shared_ptr<asio::ip::tcp::socket>& socket_ptr, const User& user);
 
         /*
-        * @brief 离开私聊房间
+        * @brief 用户连接离开私聊房间
         * @param socket_ptr socket指针
         * @return true 离开成功 | false 离开失败
         */
@@ -120,10 +108,18 @@ namespace qls
         /*
         * @brief 广播消息
         * @param message 发送的消息（非二进制数据）
-        * @param user_id 发送者user_id
+        * @param sender_user_id 发送者user_id
         * @return true 发送成功 | false 发送失败
         */
-        asio::awaitable<bool> sendData(const std::string& message, long long user_id);
+        asio::awaitable<bool> sendMessage(const std::string& message, long long sender_user_id);
+
+        /*
+        * @brief 广播提示消息
+        * @param message 发送的消息（非二进制数据）
+        * @param sender_user_id 发送者user_id
+        * @return true 发送成功 | false 发送失败
+        */
+        asio::awaitable<bool> sendTipMessage(const std::string& message);
 
     private:
         const long long m_user_id_1, m_user_id_2;
@@ -137,15 +133,25 @@ namespace qls
     public:
         struct User : public room::BaseRoom::BaseUserSetting {};
 
-        BaseGroupRoom() = default;
+        BaseGroupRoom(long long group_id);
         ~BaseGroupRoom() = default;
 
+        /*
+        * @brief 添加用户进入群聊
+        * @param user_id 用户id
+        * @return true 添加成功 | false 添加失败
+        */
         bool baseAddMember(long long user_id);
 
+        /*
+        * @brief 从群聊中移除用户
+        * @param user_id 用户id
+        * @return true 移除成功 | false 移除失败
+        */
         bool baseRemoveMember(long long user_id);
 
         /*
-        * @brief 加入私聊房间
+        * @brief 将用户连接加入群聊房间
         * @param socket_ptr socket指针
         * @param user 用户数据
         * @return true 加入成功 | false 加入失败
@@ -153,7 +159,7 @@ namespace qls
         bool joinRoom(const std::shared_ptr<asio::ip::tcp::socket>& socket_ptr, const User& user);
 
         /*
-        * @brief 离开私聊房间
+        * @brief 从群聊房间删除用户连接
         * @param socket_ptr socket指针
         * @return true 离开成功 | false 离开失败
         */
@@ -162,13 +168,30 @@ namespace qls
         /*
         * @brief 广播消息
         * @param message 发送的消息（非二进制数据）
-        * @param user_id 发送者user_id
+        * @param sender_user_id 发送者user_id
         * @return true 发送成功 | false 发送失败
         */
-        asio::awaitable<bool> sendData(const std::string& message, long long user_id);
+        asio::awaitable<bool> sendMessage(const std::string& message, long long sender_user_id);
+
+        /*
+        * @brief 发送提示消息
+        * @param message 发送的消息（非二进制数据）
+        * @return true 发送成功 | false 发送失败
+        */
+        asio::awaitable<bool> sendTipMessage(const std::string& message);
+
+        /*
+        * @brief 发送提示消息给单独某个用户
+        * @param message 发送的消息（非二进制数据）
+        * @param receiver_user_id 接收者user_id
+        * @return true 发送成功 | false 发送失败
+        */
+        asio::awaitable<bool> sendUserTipMessage(const std::string& message, long long receiver_user_id);
 
     private:
+        const long long m_group_id;
+
+        std::unordered_set<long long>   m_user_id_map;
+        mutable std::shared_mutex       m_user_id_map_mutex;
     };
 }
-
-
