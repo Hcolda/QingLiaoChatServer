b4f4825c2c978b2647c9aeea5b7fee4ed8aee1a3
曲奇Onfree <2098332747@qq.com>
2023-10-21 15:05:51 +0800

    完善class Manager的私聊房间处理部分（未完全）

--- manager.cpp
+++ manager.cpp
@@ -4,13 +4,13 @@
 
 namespace qls
 {
-    Managaer& Managaer::getGlobalManager()
+    Manager& Manager::getGlobalManager()
     {
-        static Managaer localManager;
+        static Manager localManager;
         return localManager;
     }
 
-    void Managaer::setSQLProcess(const std::shared_ptr<quqisql::SQLDBProcess>& process)
+    void Manager::setSQLProcess(const std::shared_ptr<quqisql::SQLDBProcess>& process)
     {
         if (!process)
             throw std::invalid_argument("process is nullptr");
@@ -18,8 +18,108 @@ namespace qls
         m_sqlProcess = process;
     }
 
-    void Managaer::init()
+    long long Manager::addPrivateRoom(long long user1_id, long long user2_id)
     {
+        std::unique_lock<std::shared_mutex> ul1(m_basePrivateRoom_map_mutex, std::defer_lock),
+            ul2(m_userID_to_privateRoomID_map_mutex, std::defer_lock);
+        std::lock(ul1, ul2);
+
+        // 私聊房间id
+        long long privateRoom_id;
+        {
+            /*
+            * 这里有申请sql 创建私聊房间等命令
+            */
+
+            privateRoom_id = 0;
+        }
+
+        m_basePrivateRoom_map[privateRoom_id] = std::make_shared<qls::BasePrivateRoom>(
+            user1_id, user2_id);
+        m_userID_to_privateRoomID_map[{user1_id, user2_id}] = privateRoom_id;
         
+        return privateRoom_id;
+    }
+
+    long long Manager::getPrivateRoomId(long long user1_id, long long user2_id) const
+    {
+        std::shared_lock<std::shared_mutex> sl(m_userID_to_privateRoomID_map_mutex);
+        if (m_userID_to_privateRoomID_map.find({ user1_id , user2_id }) != m_userID_to_privateRoomID_map.end())
+        {
+            return m_userID_to_privateRoomID_map.find({ user1_id , user2_id })->second;
+        }
+        else if (m_userID_to_privateRoomID_map.find({ user2_id , user1_id }) != m_userID_to_privateRoomID_map.end())
+        {
+            return m_userID_to_privateRoomID_map.find({ user2_id , user1_id })->second;
+        }
+        else throw std::invalid_argument("there is not a room matches the argument");
+    }
+
+    bool Manager::hasPrivateRoom(long long private_room_id) const
+    {
+        std::shared_lock<std::shared_mutex> sl(m_basePrivateRoom_map_mutex);
+        return m_basePrivateRoom_map.find(private_room_id) != m_basePrivateRoom_map.end();
+    }
+
+    std::shared_ptr<qls::BasePrivateRoom> Manager::getPrivateRoom(long long private_room_id) const
+    {
+        std::shared_lock<std::shared_mutex> sl(m_basePrivateRoom_map_mutex);
+        auto itor = m_basePrivateRoom_map.find(private_room_id);
+        if (itor == m_basePrivateRoom_map.end())
+            throw std::invalid_argument("there is not a room matches the argument");
+        return itor->second;
+    }
+
+    void Manager::removePrivateRoom(long long private_room_id)
+    {
+        std::unique_lock<std::shared_mutex> ul1(m_basePrivateRoom_map_mutex, std::defer_lock),
+            ul2(m_userID_to_privateRoomID_map_mutex, std::defer_lock);
+        std::lock(ul1, ul2);
+
+        auto itor = m_basePrivateRoom_map.find(private_room_id);
+        if (itor == m_basePrivateRoom_map.end())
+            throw std::invalid_argument("there is not a room matches the argument");
+
+        {
+            /*
+            * 这里有申请sql 删除私聊房间等命令
+            */
+        }
+
+        long long user1_id = itor->second->getUserID1();
+        long long user2_id = itor->second->getUserID2();
+
+        if (m_userID_to_privateRoomID_map.find({ user1_id , user2_id }) != m_userID_to_privateRoomID_map.end())
+        {
+            m_userID_to_privateRoomID_map.erase({ user1_id , user2_id });
+        }
+        else if (m_userID_to_privateRoomID_map.find({ user2_id , user1_id }) != m_userID_to_privateRoomID_map.end())
+        {
+            m_userID_to_privateRoomID_map.erase({ user2_id , user1_id });
+        }
+
+        m_basePrivateRoom_map.erase(itor);
+    }
+
+    void Manager::addGroupRoom(long long group_room_id)
+    {
+    }
+
+    bool Manager::hasGroupRoom(long long private_room_id) const
+    {
+        return false;
+    }
+
+    std::shared_ptr<qls::BaseGroupRoom> Manager::getGroupRoom(long long group_room_id) const
+    {
+        return std::shared_ptr<qls::BaseGroupRoom>();
+    }
+
+    void Manager::removeGroupRoom(long long group_room_id)
+    {
+    }
+
+    void Manager::init()
+    {
     }
 }
--- manager.h
+++ manager.h
@@ -12,18 +12,18 @@
 
 namespace qls
 {
-    class Managaer
+    class Manager
     {
     protected:
-        Managaer() = default;
-        ~Managaer() = default;
+        Manager() = default;
+        ~Manager() = default;
 
     public:
         /*
         * @brief 获取全局管理器
         * @return 管理器
         */
-        static Managaer& getGlobalManager();
+        static Manager& getGlobalManager();
 
         /*
         * @brief 设置sql
@@ -32,13 +32,82 @@ namespace qls
         void setSQLProcess(const std::shared_ptr<quqisql::SQLDBProcess>& process);
 
         /*
+        * @brief 添加私聊房间
+        * @param user1_id 用户1ID
+        * @param user2_id 用户2ID
+        * @return 创建后的私聊房间ID
+        */
+        long long addPrivateRoom(long long user1_id, long long user2_id);
+
+        /*
+        * @brief 获取私聊房间ID
+        * @param user1_id 用户1ID
+        * @param user2_id 用户2ID
+        * @return 私聊房间ID
+        */
+        long long getPrivateRoomId(long long user1_id, long long user2_id) const;
+        
+        /*
+        * @brief 是否有此私聊房间
+        * @param private_room_id 私聊房间ID
+        * @return true 有 | false 无
+        */
+        bool hasPrivateRoom(long long private_room_id) const;
+        
+        /*
+        * @brief 获取私聊房间
+        * @param private_room_id 私聊房间ID
+        * @return class BasePrivateRoom
+        */
+        std::shared_ptr<qls::BasePrivateRoom> getPrivateRoom(long long private_room_id) const;
+        
+        /*
+        * @brief 删除私聊房间
+        * @param private_room_id 私聊房间ID
+        */
+        void removePrivateRoom(long long private_room_id);
+
+        /*
+        * @brief 添加群聊房间
+        * @param group_room_id 群聊房间id
+        */
+        void addGroupRoom(long long group_room_id);
+        
+        /*
+        * @brief 是否有群聊房间
+        * @param group_room_id 群聊房间id
+        * @return true 有 | false 无
+        */
+        bool hasGroupRoom(long long group_room_id) const;
+        
+        /*
+        * @brief 获取群聊房间
+        * @param group_room_id 群聊房间id
+        * @return class BaseGroupRoom
+        */
+        std::shared_ptr<qls::BaseGroupRoom> getGroupRoom(long long group_room_id) const;
+        
+        /*
+        * @brief 删除群聊房间
+        * @param group_room_id 群聊房间id
+        */
+        void removeGroupRoom(long long group_room_id);
+
+        /*
         * @brief 初始化
         */
         void init();
 
     private:
-        std::shared_ptr<quqisql::SQLDBProcess> m_sqlProcess;
+        std::shared_ptr<quqisql::SQLDBProcess>                                  m_sqlProcess;
 
+        std::unordered_map<long long, std::shared_ptr<qls::BaseGroupRoom>>      m_baseRoom_map;
+        mutable std::shared_mutex                                               m_baseRoom_map_mutex;
 
+        std::unordered_map<long long, std::shared_ptr<qls::BasePrivateRoom>>    m_basePrivateRoom_map;
+        mutable std::shared_mutex                                               m_basePrivateRoom_map_mutex;
+
+        std::unordered_map<std::pair<long long, long long>, long long>          m_userID_to_privateRoomID_map;
+        mutable std::shared_mutex                                               m_userID_to_privateRoomID_map_mutex;
     };
 }
